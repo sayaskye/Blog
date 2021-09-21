d2fe2fad521c04a007f5e6d910be4766746b6785
Cazares <chaotears@gmail.com>
2021-09-21 13:29:07 -0600

    Ui fix to hero and post big item

--- src/components/common/PostBigItem.jsx
+++ src/components/common/PostBigItem.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const PostBigItem = ({title, excerpt, date, author, img}) => {
     return (
         <div className="bg-white/[0.02] hover:bg-white/[0.08] flex flex-row m-5 duration-500 transition">
-            <img src={img} alt="imagen"  className="w-1/3 m-2 object-cover"/>
+            <img src={img} alt="imagen"  className="w-1/3 m-2 object-cover border-main-blue border-[1px]"/>
             <div className="w-2/3 flex flex-col mx-5">
                 <h3 className="text-2xl text-main-blue uppercase font-semibold my-3 ">{title}</h3>
                 <p className="limitTextLines text-white ">{excerpt}</p>
--- src/components/layout/Hero.jsx
+++ src/components/layout/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = ({exist}) => {
             <div className="container mx-auto py-16 flex flex-col">
                 <Navbar/>
                 {exist&&
-                    <div className="bg-main-blue/[7%] rounded-2xl mt-4 flex flex-col">
+                    <div className="bg-main-blue/[5%] rounded-2xl mt-4 flex flex-col backdrop-blur-sm">
                         <span className="my-12 text-center text-main-blue text-7xl font-semibold">Andrés Cazares</span>
                         <span className="mb-12 text-center text-white text-4xl">Bienvenido a mi blog personal</span>
                     </div>
